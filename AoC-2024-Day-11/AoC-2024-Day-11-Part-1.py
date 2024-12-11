9d8fad0257c774019169f0d44e4df01762d4c372
Phantasmysteria <wallacechan58185@gmail.com>
2024-12-11 14:12:20 +0800

    Add Day 11

---
+++ AoC-2024-Day-11/AoC-2024-Day-11-Part-1.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('..')
+import AoCUtils
+import itertools, functools
+from itertools import product, accumulate
+from functools import reduce
+import numpy as np
+import re
+import math
+import copy
+
+rawInput = AoCUtils.listLines(day="11", part="1")
+
+stones = rawInput[0].split(' ')
+stones_dict = {}
+for stone in stones:
+    if stone not in stones_dict:
+        stones_dict[stone] = 0
+    stones_dict[stone] += 1
+
+def mod_dict(d, k, v):
+    if k not in d:
+        d[k] = 0
+    d[k] += v
+
+# Part 1: 25 iterations (yep that's the only change)
+for it in range(25):
+    next_stones_dict = copy.deepcopy(stones_dict)
+    for key, val in stones_dict.items():
+        if key == '0':
+            mod_dict(next_stones_dict, '1', val)
+            mod_dict(next_stones_dict, '0', -val)
+        elif not len(key) % 2:
+            mod_dict(next_stones_dict, key, -val)
+            mod_dict(next_stones_dict, key[0:len(key)//2], val)
+            mod_dict(next_stones_dict, str(int(key[len(key)//2:])), val)
+        else:
+            mod_dict(next_stones_dict, key, -val)
+            mod_dict(next_stones_dict, str(int(key)*2024), val)
+
+    stones_dict = copy.deepcopy(next_stones_dict)
+
+output = sum([v for v in stones_dict.values()])
+print(output)
